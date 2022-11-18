5e0c5ec2e1d36bfdc2b208ecc5dee9ee95acd8f9
hanksml <hank.small2020@gmail.com>
2022-11-18 16:51:40 -0500

    added SQL connection method, now working on user input methods.

--- Sharpen_Habits/Program.cs
+++ Sharpen_Habits/Program.cs
@@ -1,3 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Sharpen Habits!");
-Console.WriteLine("---------------------------");
+﻿using MySql.Data.MySqlClient;
+
+
+namespace SharpenHabits
+{
+	class Program
+	{
+		static async Task Main(string[] args) 
+		{
+			string connectionString = "Server=127.0.0.1;Database=sharpendb;Uid=root;Pwd=Jtm4sF@aMYSQ";
+			using (var connection = new MySqlConnection(connectionString))
+			{
+				await connection.OpenAsync();
+
+				using (var command = new MySqlCommand())
+				{
+					command.Connection = connection;
+					command.CommandText =
+						@"CREATE TABLE IF NOT EXISTS habits (
+							Id INTEGER PRIMARY KEY AUTO_INCREMENT,
+							Date TEXT,
+							Quantity INTEGER
+						)";
+					await command.ExecuteNonQueryAsync();
+
+					connection.Close();	
+				}
+			}
+
+            Console.WriteLine("Welcome to Sharpen Habits!");
+            Console.WriteLine("---------------------------");
+        }
+
+		static void GetUserInput()
+		{
+			Console.Clear();
+			bool closeApp = false;
+			while (closeApp == false)
+			{
+
+			}
+		}
+        
+	}
+}
+
+
