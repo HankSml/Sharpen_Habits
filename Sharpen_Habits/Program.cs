e2f7399116b4959988b87259d85b843874915aa8
hanksml <hank.small2020@gmail.com>
2022-11-19 16:03:15 -0500

    Adding ViewAll method and refactoring code to use 2 classes

--- Sharpen_Habits/Program.cs
+++ Sharpen_Habits/Program.cs
@@ -1,51 +1,33 @@
-﻿using MySql.Data.MySqlClient;
+﻿using static Sharpen_Habits.Query;
 
 
-namespace SharpenHabits
+namespace Sharpen_Habits
 {
 	class Program
 	{
-		static string connectionString = "Server=127.0.0.1;Database=sharpendb;Uid=root;Pwd=Jtm4sF@aMYSQ";
-        static async Task Main(string[] args) 
+		static async void Main(string[] args)
 		{
-			using (var connection = new MySqlConnection(connectionString))
-			{
-				await connection.OpenAsync();
+			await Sharpen_Habits.Query.Init();
 
-				using (var command = new MySqlCommand())
-				{
-					command.Connection = connection;
-					command.CommandText =
-						@"CREATE TABLE IF NOT EXISTS habits (
-							Id INTEGER PRIMARY KEY AUTO_INCREMENT,
-							Date TEXT,
-							Quantity INTEGER
-						)";
-					await command.ExecuteNonQueryAsync();
+			await GetUserInput();
+		}
 
-					connection.Close();	
-				}
-			}
-
-            Console.WriteLine("Welcome to Sharpen Habits!");
-            Console.WriteLine("---------------------------");
-			GetUserInput();
-        }
-
-		static void GetUserInput()
+		public static async Task GetUserInput()
 		{
 			Console.Clear();
 			bool closeApp = false;
 			while (closeApp == false)
 			{
-				Console.WriteLine("Menu:");
-                Console.WriteLine("Please select an option to begin");
-                Console.WriteLine("Press 0 to close the application");
-                Console.WriteLine("Press 1 to view all habits");
-                Console.WriteLine("Press 2 to insert a habit");
-                Console.WriteLine("Press 3 to delete a habit");
-                Console.WriteLine("Press 4 to update a habit");
-                Console.WriteLine("--------------------------------------\n");
+                Console.WriteLine("Welcome to Sharpen Habits!");
+                Console.WriteLine("---------------------------\n");
+                Console.WriteLine("Menu:");
+				Console.WriteLine("Please select an option to begin");
+				Console.WriteLine("Press 0 to close the application");
+				Console.WriteLine("Press 1 to view all habits");
+				Console.WriteLine("Press 2 to insert a habit");
+				Console.WriteLine("Press 3 to delete a habit");
+				Console.WriteLine("Press 4 to update a habit");
+				Console.WriteLine("--------------------------------------\n");
 
 				string selectedOption = Console.ReadLine();
 
@@ -55,69 +37,23 @@ namespace SharpenHabits
 						Console.WriteLine("Goodbye!");
 						closeApp = true;
 						break;
-					//case 1:
-					//	GetAllHabits();
-					//	break;
-					case "2":
-						InsertHabit();
+					case "1":
+						await Sharpen_Habits.Query.GetAllHabits();
 						break;
-					//case 3:
-					//	DeleteHabit();
-					//	break;
-					//case 4:
-					//	UpdateHabit();
-					//	break;
-					//default:
-					//	break;
+					case "2":
+						await Sharpen_Habits.Query.InsertHabit();
+						break;
+					case "3":
+						await Sharpen_Habits.Query.DeleteHabit();
+						break;
+					case "4":
+						await Sharpen_Habits.Query.UpdateHabit();
+						break;
+					default:
+						break;
 				}
-			}
+            }
 		}
-		private static async void InsertHabit()
-		{
-			string date = GetDateInput();
-
-			int quantity = GetNumberInput("Please insert the number of cups of water per day");
-
- 
-			using (var connection = new MySqlConnection(connectionString))
-			{
-				await connection.OpenAsync();
-
-				using (var command = new MySqlCommand())
-				{
-					command.Connection = connection;
-					command.CommandText =
-						$"INSERT INTO habits(date, quantity) VALUES('{date}', {quantity})";
-
-					await command.ExecuteNonQueryAsync();
-
-					connection.Close();
-				}
-			}
-        }
-		internal static string GetDateInput()
-		{
-			Console.WriteLine("Please insert the date: (Format: dd-mm-yy). Type 0 to return to the main menu");
-
-			string input = Console.ReadLine();
-
-			if (input == "0") GetUserInput();
-
-			return input;
-		}
-		internal static int GetNumberInput(string message)
-		{
-			Console.WriteLine(message);
-
-			string rawInput = Console.ReadLine();
-
-			if (rawInput == "0") GetUserInput();
-
-			int finalInput = Convert.ToInt32(rawInput);
-
-			return finalInput;
-		}
-        
 	}
 }
 
---
+++ Sharpen_Habits/Query.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySqlConnector;
+using static Sharpen_Habits.Program;
+
+namespace Sharpen_Habits
+{
+    class Query
+    {
+        static string connectionString = "Server=127.0.0.1;Database=sharpendb;Uid=root;Pwd=Jtm4sF@aMYSQ";
+        public static async Task Init()
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                using (var command = new MySqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText =
+                        @"CREATE TABLE IF NOT EXISTS habits (
+							Id INTEGER PRIMARY KEY AUTO_INCREMENT,
+							Date TEXT,
+							Quantity INTEGER
+						)";
+                    await command.ExecuteNonQueryAsync();
+
+                    connection.Close();
+                }
+            }
+        }
+        public static async Task InsertHabit()
+        {
+            string date = GetDateInput();
+
+            int quantity = GetNumberInput("Please insert the number of cups of water per day");
+
+
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                using (var command = new MySqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText =
+                        $"INSERT INTO habits(date, quantity) VALUES('{date}', {quantity})";
+
+                    await command.ExecuteNonQueryAsync();
+
+                    connection.Close();
+                }
+            }
+        }
+        public static async Task GetAllHabits() 
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                using (var command = new MySqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText =
+                        $"SELECT * FROM habits";
+
+                    await command.ExecuteNonQueryAsync();
+
+                    List<Habit> tableData = new();
+
+                    MySqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.HasRows) 
+                    { 
+                        while (reader.Read())
+                        {
+                            tableData.Add(
+                                new Habit
+                                {
+                                    Id = reader.GetInt32(0),
+                                    Date = DateTime.ParseExact(reader.GetString(1), "dd-MM-yy", new CultureInfo("en-US")),
+                                    Quantity = reader.GetInt32(2),
+                                });
+                        }
+                    } else 
+                    {
+                        Console.WriteLine("No rows found");
+                    }
+
+                    connection.Close();
+
+                    Console.WriteLine("-------------------------\n");
+                    foreach (var row in tableData)
+                    {
+                        Console.WriteLine($"{row.Id} - {row.Date.ToString("dd-MMM-yyyy")} - Quantity: {row.Quantity}");
+                    }
+                    Console.WriteLine("-------------------------\n");
+                }
+            }
+        }
+        public static async Task DeleteHabit() 
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+            }
+        }
+        public static async Task UpdateHabit()
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+            }
+        }
+        internal static string GetDateInput()
+        {
+            Console.WriteLine("Please insert the date: (Format: dd-mm-yy). Type 0 to return to the main menu");
+
+            string input = Console.ReadLine();
+
+            if (input == "0") Sharpen_Habits.Program.GetUserInput();
+
+            return input;
+        }
+        internal static int GetNumberInput(string message)
+        {
+            Console.WriteLine(message);
+
+            string rawInput = Console.ReadLine();
+
+            if (rawInput == "0") GetUserInput();
+
+            int finalInput = Convert.ToInt32(rawInput);
+
+            return finalInput;
+        }
+
+    }
+
+    public class Habit
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int  Quantity { get; set;}
+    }
+}
+
